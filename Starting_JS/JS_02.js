1ab61cc25a9ac997eb299aacf7bf2c84226fdaab
aashishthakurcodes <j9183208@gmail.com>
2022-12-19 21:03:40 +0530

    JS_02.js created

---
+++ Starting_JS/JS_02.js
@@ -0,0 +1,12 @@
+// Array and its method:-
+
+// Declaration Of Array:-
+// 1.JS Array literal
+let arr1=["Aashish","Arun","Shivani","Hariom", 45 ,14]
+console.log(arr1)
+
+// 2. Directly new keyword
+let x=new Array("Aashish","Keshu","Jitesh",85,96,751,4562)
+console.log(x);
+
+// Array method
